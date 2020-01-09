8a8a5792294cf6cc294712bc6dcbc48eba217433
Exos Nguyen <esoft288@gmail.com>
2020-01-09 22:33:57 +0700

    2020-01-09

---
+++ src/main/java/com/ebot/ebotsite/JAXRSConfiguration.java
@@ -0,0 +1,13 @@
+package com.ebot.ebotsite;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ * Configures JAX-RS for the application.
+ * @author Juneau
+ */
+@ApplicationPath("resources")
+public class JAXRSConfiguration extends Application {
+    
+}
